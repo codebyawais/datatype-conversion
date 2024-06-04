0d46c30d6f5a4dfd3d98a27cbaa18e0fe21903d8
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-06-04 22:29:00 +0500

    Add files via upload

---
+++ datatype-conversion.js
@@ -0,0 +1,60 @@
+// let score = "33aaa";
+// let score = "33"
+// let score = 33;
+// let score = null; 
+// let score = undefined\
+// let score = true;
+// let score = "false"; //nan
+// let score = false; 
+// console.log(typeof score); //number
+// console.log(typeof(score)); //number
+// console.log(typeof("score"));//string
+
+
+
+// let valueinnumber = Number(score)
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
+
+// 33abc provide nan
+// null provide     0
+// undefined provide a    nan
+
+
+
+// let valueisloggedin = "awais";
+// let Booleanvalueisloggedin =Boolean( valueisloggedin);
+// console.log(Booleanvalueisloggedin);
+
+
+// let valueisloggedin = "1";
+// let Booleanvalueisloggedin =Boolean( valueisloggedin);
+// console.log(Booleanvalueisloggedin);
+
+
+
+
+
+//    false
+// let valueisloggedin = "";
+// let Booleanvalueisloggedin =Boolean( valueisloggedin);
+// console.log(Booleanvalueisloggedin);
+
+// let valueisloggedin = 0;
+// let Booleanvalueisloggedin =Boolean( valueisloggedin);
+// console.log(Booleanvalueisloggedin);
+
+
+// let valueisloggedin = false;
+// let Booleanvalueisloggedin =Boolean( valueisloggedin);
+// console.log(Booleanvalueisloggedin);
+
+
+
+
+
+
+let somenumber = "11"
+let stingnumber= String(somenumber)
+console.log(somenumber);
+console.log(typeof somenumber);
